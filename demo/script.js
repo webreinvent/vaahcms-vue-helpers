5d085c6fb4354372940ad3b8311d416b7203b01d
Pradeep Kumar <webreinvent@gmail.com>
2019-07-11 14:26:01 +0530

    v0.0.3

--- demo/script.js
+++ demo/script.js
@@ -16,7 +16,7 @@ const app = new Vue({
 
         //-----------------------------------------------------------
         getHelper: function () {
-            this.$vaahcms.checkConsole();
+            this.$vaahcms.console('test');
             console.log('test');
         },
         //---------------------------------------------------------------------
